b32a13200f1401649f6148f15a58ca540a2e1bfa
Iperalex <iper_alex@hotmail.it>
2022-10-18 17:02:23 +0200

    solana payment with wallet adapter works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { clusterApiUrl } from '@solana/web3.js'
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { LedgerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
@@ -24,6 +24,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const wallets = [
     new PhantomWalletAdapter(),
     new SolflareWalletAdapter({ network }),
+    new LedgerWalletAdapter(),
   ];
 
   return (
